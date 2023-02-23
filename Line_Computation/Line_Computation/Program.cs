1864852c57d9e7a30ad0203374513582e28c760d
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-23 02:05:51 -0500

    [Ruchita] Add: Comparsion Between two lines

--- Line_Computation/Line_Computation/Program.cs
+++ Line_Computation/Line_Computation/Program.cs
@@ -17,11 +17,36 @@ namespace Line_Computation
             Console.WriteLine("Enter the value of Y1 Coordinate:");
             int y1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the value of Y2 Coordinate");
+            Console.WriteLine("Enter the value of Y2 Coordinate:");
             int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double LengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
-            Console.WriteLine("Length of Line is :" + LengthOfLine);
+            Console.WriteLine("Enter the value of X3 Coordinate:");
+            int x3= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of X4 Coordinate:");
+            int x4= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y3 coordinate:");
+            int y3= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value of Y4 coordinate: ");
+            int y4= Convert.ToInt32(Console.ReadLine());
+
+            double LengthOfLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of Line1 is :" + LengthOfLine1);
+
+            double LengthOfLine2=Math.Sqrt(Math.Pow((x4-x3),2)+Math.Pow((y4-y3),2));
+            Console.WriteLine("Length of Line2 is:" + LengthOfLine2);
+
+            if (LengthOfLine1.Equals(LengthOfLine2)== true)
+            {
+                Console.WriteLine("Length of line1 and Line2 are equal");
+            }
+            else
+            {
+                Console.WriteLine("Length of Line1 and Line2 are not equal");
+            }
+
             Console.ReadLine();
         }
 
